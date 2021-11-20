6ded1be5ef71ee7f8cc1716ccd32db7e6ff98b98
pexure <pexure@gmail.com>
2021-11-20 21:29:04 +0800

    m

--- src/dna_dataset_v2.py
+++ src/dna_dataset_v2.py
@@ -82,8 +82,6 @@ def get_DNA_loader(root, device="cpu", batch_size=128):
         src_left = torch.stack(src_left, dim=-1).contiguous().to(device)  # (max_t, max_len, 4, bsz)
         src_right = torch.stack(src_right, dim=-1).contiguous().to(device)
         tgt = torch.stack(tgt, dim=-1).contiguous().to(device)  # (max_len, bsz)
-        print(src_left.shape, src_right.shape)
-        exit()
         return src_left, src_right, tgt
 
     train_ds = DNA_dataset_v2(root=root, split="train")
